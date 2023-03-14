da61bf45b01775ec7f4c8a90fa0e60f6d064f239
ROMAN <junk0410448@mail.ru>
2023-03-14 15:50:13 +0700

    Zadacha1

---
+++ Zadacha1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите первое число: ");
+int firstValue = Convert.ToInt32(Console.ReadLine()); // Перевели строчку в формат целого числа
+
+Console.Write("Введите второе число: ");
+int secondValue = Convert.ToInt32(Console.ReadLine());
+if (firstValue > secondValue)
+{  Console.WriteLine("Первое число " + firstValue + " больше чем второе " + secondValue);
+}
+else
+{
+    Console.WriteLine("Второе число " + secondValue + " больше чем первое " + firstValue);
+}
